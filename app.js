2a540f2c452c73d4414af7956fed4439c96b96df
Shady Developer <shady.developer@proton.me>
2022-10-13 21:19:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+import { } from 'dotenv/config';
+import express from 'express';
+import { Telegraf } from 'telegraf';
+
+const bot = new Telegraf(process.env.TelegramBotToken);
+const app = express();
+
+// Set the bot API endpoint
+app.use(
+  await bot.createWebhook({
+    domain: process.env.WebhookDomain,
+  })
+);
+
+bot.on('channel_post', async (ctx) => {
+  const text = ctx.channelPost.text;
+  if (!text.match(/Пут[и,і]н/gi)) return
+
+  const editedText = text.replace(/Пут[и,і]н/gi, function (match) {
+    const firstLetter = match[0] === 'П' ? 'Х' : 'х';
+    const rest = match.slice(2);
+    return `${firstLetter}ую${rest}`;
+  });
+
+  await ctx.telegram.editMessageText(
+    ctx.channelPost.chat.id,
+    ctx.channelPost.message_id,
+    undefined,
+    editedText
+  );
+
+});
+
+const PORT = process.env.port || 9630
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
