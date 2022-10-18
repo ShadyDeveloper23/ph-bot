521fc3fa8af9b1503030af27e79c45f1278bca5f
Shady Developer <shady.developer@proton.me>
2022-10-18 16:42:25 +0300

    changed regexp3

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(
 
 bot.on('channel_post', async (ctx) => {
   const text = ctx.channelPost.text;
-  if (!text.match(/(?<=[\s,.:;"']|^)[пП]ут[и,і]н)/gm)) {
+  if (!text.match(/(?<=[\s,.:;"']|^)[пП]ут[и,і]н/gm)) {
     console.log('there is no match')
     return
 
   } 
 
-  const editedText = text.replace(/(?<=[\s,.:;"']|^)[пП]ут[и,і]н)/gm, function (match) {
+  const editedText = text.replace(/(?<=[\s,.:;"']|^)[пП]ут[и,і]н/gm, function (match) {
     const firstLetter = match[0] === 'П' ? 'Х' : 'х';
     const rest = match.slice(2);
     return `${firstLetter}ую${rest}`;
